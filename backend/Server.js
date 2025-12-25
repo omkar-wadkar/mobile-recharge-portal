37c29611152c51e68593018e1c6bd2118508ca13
omkar-wadkar <omkar.a.wadkar@gmail.com>
2025-12-25 16:12:11 +0530

    Comment app.listen for vercel deploy

--- backend/Server.js
+++ backend/Server.js
@@ -26,4 +26,5 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.error('MongoDB Connection Error:', err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
